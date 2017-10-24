2111f22ba05ab91f8ef41b43fdbdec01ab21bc0c
Mehdi Khaleghian <mehdi.khaleghian@readify.net>
2017-10-24 12:36:05 +1100

    Write to blob storage

--- AzureFunctionsFundamental/GenerateLicenceFile.cs
+++ AzureFunctionsFundamental/GenerateLicenceFile.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -6,9 +10,16 @@ namespace AzureFunctionsFundamental
     public static class GenerateLicenceFile
     {
         [FunctionName("GenerateLicenceFile")]
-        public static void Run([QueueTrigger("order")]Order order, TraceWriter log)
+        public static void Run([QueueTrigger("order")]Order order, TraceWriter log,
+            [Blob("licences/{rand-guid}.lic")] TextWriter outputBlob)
         {
             log.Info($"Received an order: Order {order.OrderId}, product {order.ProductId}, email {order.Email}");
+            outputBlob.WriteLine($"OrderId: {order.OrderId}");
+            outputBlob.WriteLine($"Email: {order.Email}");
+            outputBlob.WriteLine($"ProductId: {order.ProductId}");
+            outputBlob.WriteLine($"Purchase Date: {DateTime.UtcNow}");
+            var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(order.Email + "secret"));
+            outputBlob.WriteLine($"SecretCode: {BitConverter.ToString(hash).Replace("-", "")}");
         }
     }
 }
