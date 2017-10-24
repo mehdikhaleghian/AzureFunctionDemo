856f4fc873fb2f6b02b174220bd135ea8af5dcfa
Mehdi Khaleghian <mehdi.khaleghian@readify.net>
2017-10-24 14:36:41 +1100

    use IBinder instead of hard coded Blob

--- AzureFunctionsFundamental/GenerateLicenceFile.cs
+++ AzureFunctionsFundamental/GenerateLicenceFile.cs
@@ -11,15 +11,19 @@ namespace AzureFunctionsFundamental
     {
         [FunctionName("GenerateLicenceFile")]
         public static void Run([QueueTrigger("order")]Order order, TraceWriter log,
-            [Blob("licences/{rand-guid}.lic")] TextWriter outputBlob)
+            /*[Blob("licences/{rand-guid}.lic")] TextWriter outputBlob*/
+            IBinder binder)
         {
             log.Info($"Received an order: Order {order.OrderId}, product {order.ProductId}, email {order.Email}");
-            outputBlob.WriteLine($"OrderId: {order.OrderId}");
-            outputBlob.WriteLine($"Email: {order.Email}");
-            outputBlob.WriteLine($"ProductId: {order.ProductId}");
-            outputBlob.WriteLine($"Purchase Date: {DateTime.UtcNow}");
-            var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(order.Email + "secret"));
-            outputBlob.WriteLine($"SecretCode: {BitConverter.ToString(hash).Replace("-", "")}");
+            using (var outputBlob = binder.Bind<TextWriter>( new BlobAttribute($"licences/{order.OrderId}.lic")))
+            {
+                outputBlob.WriteLine($"OrderId: {order.OrderId}");
+                outputBlob.WriteLine($"Email: {order.Email}");
+                outputBlob.WriteLine($"ProductId: {order.ProductId}");
+                outputBlob.WriteLine($"Purchase Date: {DateTime.UtcNow}");
+                var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(order.Email + "secret"));
+                outputBlob.WriteLine($"SecretCode: {BitConverter.ToString(hash).Replace("-", "")}");
+            }
         }
     }
 }
